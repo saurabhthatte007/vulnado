db6e4dc0be9e57f4b2f3074ca52e09f98918ee9e
saurabhthatte007 <139212657+saurabhthatte007@users.noreply.github.com>
2024-11-20 12:41:49 +0530

    Update Postgres.java

--- src/main/java/com/scalesec/vulnado/Postgres.java
+++ src/main/java/com/scalesec/vulnado/Postgres.java
@@ -101,17 +101,17 @@ public class Postgres {
        }
     }
 
-    private static void insertComment(String username, String body) {
-        String sql = "INSERT INTO comments (id, username, body, created_on) VALUES (?, ?, ?, current_timestamp)";
-        PreparedStatement pStatement = null;
-        try {
-            pStatement = connection().prepareStatement(sql);
-            pStatement.setString(1, UUID.randomUUID().toString());
-            pStatement.setString(2, username);
-            pStatement.setString(3, body);
-            pStatement.executeUpdate();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+    private static void insertComment(String username, String password) {
+       String sql = "INSERT INTO users (user_id, username, password, created_on) VALUES (?, ?, ?, current_timestamp)";
+       PreparedStatement pStatement = null;
+       try {
+          pStatement = connection().prepareStatement(sql);
+          pStatement.setString(1, UUID.randomUUID().toString());
+          pStatement.setString(2, username);
+          pStatement.setString(3, md5(password));
+          pStatement.executeUpdate();
+       } catch(Exception e) {
+         e.printStackTrace();
+       }
     }
 }
